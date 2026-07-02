67bf21d94f61b29db2e9a50b2ba4897108b7c4b7
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2026-07-02 12:10:15 -0400

    Implement fibonacci sequece using recursion

--- fibonacci.js
+++ fibonacci.js
@@ -17,3 +17,23 @@ function fibs(n) {
 
   return sequence;
 }
+
+function fibsRec(n) {
+  if (n === 0) {
+    return [];
+  }
+  if (n === 1) {
+    return [1];
+  }
+  if (n === 2) {
+    return [0, 1];
+  }
+  if (n > 2) {
+    //the current array is always just the previous array appended with the sum of the last two array elements
+    const sequence = fibsRec(n - 1);
+    const length = sequence.length;
+    const sum = sequence[length - 1] + sequence[length - 2];
+    sequence.push(sum);
+    return sequence;
+  }
+}
