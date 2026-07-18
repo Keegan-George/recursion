bb29c3d6191bbc439aa97c22e24c3b27eec83a07
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2026-07-18 10:21:07 -0400

    Implement merge and mergeSort functions

--- merge-sort.js
+++ merge-sort.js
@@ -1,3 +1,38 @@
-function mergeSort(){
-    
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid, arr.length);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(arr1, arr2) {
+  const arr3 = [];
+
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      arr3.push(arr1[i]);
+      i++;
+    } else {
+      arr3.push(arr2[j]);
+      j++;
+    }
+  }
+
+  if (i >= arr1.length) {
+    arr3.push(...arr2.slice(j, arr2.length));
+  }
+
+  if (j >= arr2.length) {
+    arr3.push(...arr1.slice(i, arr1.length));
+  }
+
+  return arr3;
 }
